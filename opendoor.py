7b7f34006c69e2fc2080513bc8d00f96929260c3
Stanislav WEB <stanisov@gmail.com>
2016-07-09 06:16:55 +0300

    [ADD] Basic structure

---
+++ opendoor.py
@@ -0,0 +1,18 @@
+#    OpenDoor Web Vulnerability Scanner
+#    Copyright (C) 2016  Stanislav Menshov
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    Development Team:
+#
+#    Stanislav Menshov (Stanislav WEB) since version 1.0
