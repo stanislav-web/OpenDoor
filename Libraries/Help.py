ffda76fab27b9e408cda6a5097a355d42f0afbf6
Stanislav WEB <stanisov@gmail.com>
2016-07-12 02:59:14 +0300

    [ADD] Help init console (update)

--- Libraries/Help.py
+++ Libraries/Help.py
@@ -3,11 +3,11 @@ import argparse
 class Help:
     """Console helper class"""
     def __init__(self):
-        parser = argparse.ArgumentParser(
-            description='''My Description. And what a lovely description it is. ''',
-            epilog="""All's well that ends well.""")
-        parser.add_argument('--foo', type=int, default=42, help='FOO!')
-        parser.add_argument('bar', nargs='*', default=[1, 2, 3], help='BAR!')
+        parser = argparse.ArgumentParser(description=__doc__,
+                                         formatter_class=argparse.RawDescriptionHelpFormatter)
+        parser.add_argument('-u', '--url', help="URL or page to scan; -u http://example.com")
+        parser.add_argument('-t', '--threads', help="Allowed threads")
+        parser.add_argument('-c', '--connections', default='30',
+                            help="Set the max number of simultaneous connections allowed, default=30")
         args = parser.parse_args()
-
-
+        return args
