3abc692f29b31e2dbe5e303de9bbe122520fbe9d
Stanislav WEB <stanisov@gmail.com>
2016-08-18 13:41:17 +0300

    [ADD] Some tests

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -16,11 +16,22 @@ class TestFilter(unittest.TestCase):
         else:
             return True
 
-
     def test_threads(self):
         threads = Filter.threads(0)
         self.assertTrue(self.__isNumeric(threads));
         self.assertGreater(threads, 0)
+
+    def test_check(self):
+        check = Filter.check('unknown')
+        self.assertEqual(check, 'directories')
+
+    def test_debug(self):
+        debug = Filter.debug(1)
+        self.assertEqual(debug, 1)
+
+    def test_delay(self):
+        delay = Filter.delay(1)
+        self.assertEqual(delay, 1)
 
 if __name__ == "__main__":
     unittest.main()
