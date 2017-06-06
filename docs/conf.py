b49539849beb53dd01092430cff2dfdc3fd11faf
Stanislav WEB <stanisov@gmail.com>
2017-06-06 16:31:52 +0300

    [Update]#  Update docs [skip-ci]

--- docs/conf.py
+++ docs/conf.py
@@ -128,9 +128,9 @@ html_theme = 'default'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
- html_theme_options = {
+html_theme_options = {
     "base_url": "http://opendoor.readthedocs.io/"
- }
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
