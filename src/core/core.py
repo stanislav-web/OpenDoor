2f879b1fb2e78d3faa8976808ed92cc8917bf428
stanislav-web <stanisov@gmail.com>
2017-06-22 01:00:03 +0300

    # [v3.3.37-rc] Fix IndexOf error (update dictionaries)

--- src/core/core.py
+++ src/core/core.py
@@ -22,7 +22,7 @@ CoreConfig = {
         'repository': 'git@github.com:stanislav-web/OpenDoor.git',
         'remote_version': 'https://raw.githubusercontent.com/stanislav-web/OpenDoor/reformat/VERSION',
         'license': 'License: GNU General Public License',
-        'version': '3.3.36-rc',
+        'version': '3.3.37-rc',
         'documentations': 'https://opendoor.readthedocs.org',
         'required_versions': {
             'minor': '3.3',
