2f6c74a6f76bef01d8ceac946bb628c5c939797d
stanislav.menshykh <stanisov@gmail.com>
2021-02-23 19:45:35 +0200

    [deploy] Update Travis

--- docs/conf.py
+++ docs/conf.py
@@ -135,7 +135,19 @@ html_theme = 'default'
 # documentation.
 #
 html_theme_options = {
-    "base_url": "http://opendoor.readthedocs.io/"
+    "base_url": "http://opendoor.readthedocs.io/",
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
